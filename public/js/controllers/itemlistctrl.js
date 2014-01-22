a3bebc6e3bd19f5e95aa98da595098aa9c1ecbf9
Marcelo Alves <selfmadecelo@gmail.com>
2014-01-21 21:35:02 -0800

    Moved some stuff around, added new item list

--- public/js/controllers/itemlistctrl.js
+++ public/js/controllers/itemlistctrl.js
@@ -1,5 +1,6 @@
-function ItemListCtrl($scope, $http) {
+angular.module('starboundItems')
+.controller('itemlistCtrl', function($scope, $http) {
     $http.get('items.json').success(function(data) {
     $scope.items = data.items;
   });
-}
+});
