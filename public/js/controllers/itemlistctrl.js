e981f171fa7d15f90de9af733e6632bd8e2eed6b
Marcelo Alves <selfmadecelo@gmail.com>
2014-08-11 22:57:59 -0700

    Change search url

--- public/js/controllers/itemlistctrl.js
+++ public/js/controllers/itemlistctrl.js
@@ -2,7 +2,7 @@ app.controller('itemlistCtrl', function($scope, $http, $timeout) {
   $scope.searchText = null;
   $scope.change = function(text) {
       valtosend = $scope.searchText;
-      $http.get('http://dry-brushlands-8923.herokuapp.com/api/search/' + valtosend).then(function(result){
+      $http.get('http://starbounditems.herokuapp.com/api/search/' + valtosend).then(function(result){
           $scope.entries = result.data;
       });
     };
