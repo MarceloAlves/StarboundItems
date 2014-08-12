c00ebe6bfc8e5fd550f45d8595c7d2d3068c3a34
Marcelo Alves <selfmadecelo@gmail.com>
2014-08-11 21:48:47 -0700

    Use RethinkDB instead of loading JSON file

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('starboundItems', ['pasvaz.bindonce']);
+var app = angular.module('starboundItems', []);
--- public/js/controllers/itemlistctrl.js
+++ public/js/controllers/itemlistctrl.js
@@ -1,13 +1,9 @@
-app.factory('StarboundItems', function ($http) {
-    var StarboundItems = {};
-    $http.get('items.json').success(function(data) {
-    StarboundItems.items = data.items;
-  });
-    return StarboundItems;
-});
-
-app.controller('itemlistCtrl', function($scope, StarboundItems) {
-    $scope.starbound = StarboundItems;
-
-    $scope.viewList = 'false';
+app.controller('itemlistCtrl', function($scope, $http, $timeout) {
+  $scope.searchText = null;
+  $scope.change = function(text) {
+      valtosend = $scope.searchText;
+      $http.get('http://dry-brushlands-8923.herokuapp.com/api/search/' + valtosend).then(function(result){
+          $scope.entries = result.data;
+      });
+    };
 });
