953d9ac6a6b4ae62cec2f21162c2243fe4f7daf7
Marcelo Alves <selfmadecelo@gmail.com>
2014-08-15 00:31:38 -0700

    Always forget to change URL back

--- public/js/controllers/itemlistctrl.js
+++ public/js/controllers/itemlistctrl.js
@@ -4,7 +4,7 @@ app.controller('itemlistCtrl', function($scope, $http, $timeout) {
   $scope.change = function(text) {
       valtosend = $scope.searchText;
       nightly = $scope.nightly ? 'nightly/' : ''
-      $http.get('http://localhost:9393/api/search/'+ nightly + valtosend).then(function(result){
+      $http.get('http://starbounditems.herokuapp.com/api/search/'+ nightly + valtosend).then(function(result){
           $scope.entries = result.data;
       });
     };
