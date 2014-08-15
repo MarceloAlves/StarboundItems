e65ce4784d7c86a6d309677afa6d6e661af72de9
Marcelo Alves <selfmadecelo@gmail.com>
2014-08-15 00:30:25 -0700

    Super quick change to include items from nightly builds

--- public/js/controllers/itemlistctrl.js
+++ public/js/controllers/itemlistctrl.js
@@ -1,8 +1,10 @@
 app.controller('itemlistCtrl', function($scope, $http, $timeout) {
   $scope.searchText = null;
+  $scope.nightly = false;
   $scope.change = function(text) {
       valtosend = $scope.searchText;
-      $http.get('http://starbounditems.herokuapp.com/api/search/' + valtosend).then(function(result){
+      nightly = $scope.nightly ? 'nightly/' : ''
+      $http.get('http://localhost:9393/api/search/'+ nightly + valtosend).then(function(result){
           $scope.entries = result.data;
       });
     };
