9db0283b3c5617438e51e8a8385f918e80f5b223
Marcelo Alves <selfmadecelo@gmail.com>
2015-03-20 17:21:57 -0700

    Removed hardcoded url Fixes #10

--- public/js/controllers/itemlistctrl.js
+++ public/js/controllers/itemlistctrl.js
@@ -2,7 +2,7 @@ app.controller('itemlistCtrl', function($scope, $http, $timeout) {
   $scope.searchText = null;
   $scope.change = function(text) {
       valtosend = $scope.searchText;
-      $http.get('http://starbounditems.herokuapp.com/api/search/' + valtosend).then(function(result){
+      $http.get(window.location.origin + '/api/search/' + valtosend).then(function(result){
           $scope.entries = result.data;
       });
     };
