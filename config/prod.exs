0c639ea2d315efa31060cabde8ae6e97dc3078b9
Marcelo Alves <selfmadecelo@gmail.com>
2016-11-26 18:59:09 -0800

    Remove HTTPS (#31)
    Remove HTTPS

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :starbound_items, StarboundItems.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "arcane-depths-38791.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "http", host: "starbounditems.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
