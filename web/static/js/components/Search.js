be254fcdece77754d3d740662b9e0e4ea1b17cac
Marcelo Alves <selfmadecelo@gmail.com>
2017-01-13 16:01:27 -0800

    Add temporary survey link

--- web/static/js/components/Search.js
+++ web/static/js/components/Search.js
@@ -17,27 +17,37 @@ const Search = (props) => {
   }
 
   let renderInfoRow = () => (
-    <div className="row">
-      <div className="col-md-4">
-        <div className="well">
-          <h4>What is this?</h4>
-          <p>This website allows you to search for items in the game Starbound. Using the <code>/spawnitem</code> command, you can generate an item in-game. Learn more about <a href="http://starbounder.org/Commands" target="_blank">in-game commands</a>.</p>
-          <p>Feel free to submit any feedback or suggestions on <a href="https://github.com/marceloalves/starbounditems/issues" target="_blank">Github</a>.</p>
+    <div>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="well bg-danger text-center">
+            <p>Hey there! I'm trying to decide on what to do next with this site. If you have a few minutes and wouldn't mind filling out a survey
+            it would be super helpful. <br /><a href="https://docs.google.com/forms/d/e/1FAIpQLScNGkSFEJgt6tZxc6jUli72WUNg3VxMwTaYqAk4r5KMt2vvYw/viewform" target="_blank">Head on over to this link.</a><br />Thanks!</p>
+          </div>
         </div>
       </div>
-      <div className="col-md-4">
-        <div className="well">
-          <h4>What's changed recently?</h4>
-          <ul>
-            <li>1.0 Items Added</li>
-          </ul>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="well">
+            <h4>What is this?</h4>
+            <p>This website allows you to search for items in the game Starbound. Using the <code>/spawnitem</code> command, you can generate an item in-game. Learn more about <a href="http://starbounder.org/Commands" target="_blank">in-game commands</a>.</p>
+            <p>Feel free to submit any feedback or suggestions on <a href="https://github.com/marceloalves/starbounditems/issues" target="_blank">Github</a>.</p>
+          </div>
         </div>
-      </div>
-      <div className="col-md-4">
-        <div className="well">
-          <h4>How can I support this?</h4>
-          <p>This site is supported by ads to keep the server going. If you'd like to contribute to the site please visit the <a href="https://github.com/MarceloAlves/StarboundItems">GitHub Repo</a>.</p>
-          <p>I haven't spent a lot of time parsing new objects. Some help on a decent parsing script would be great. Get in touch in the GitHub issues. Thanks</p>
+        <div className="col-md-4">
+          <div className="well">
+            <h4>What's changed recently?</h4>
+            <ul>
+              <li>1.0 Items Added</li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="well">
+            <h4>How can I support this?</h4>
+            <p>This site is supported by ads to keep the server going. If you'd like to contribute to the site please visit the <a href="https://github.com/MarceloAlves/StarboundItems">GitHub Repo</a>.</p>
+            <p>I haven't spent a lot of time parsing new objects. Some help on a decent parsing script would be great. Get in touch in the GitHub issues. Thanks</p>
+          </div>
         </div>
       </div>
     </div>
