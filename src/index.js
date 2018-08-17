7db7c115d15e2af1dd8a0fe7eec87afcb8b2866f
Marcelo Alves <selfmadecelo@gmail.com>
2018-08-16 21:01:32 -0700

    Add Sentry

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import Analytics from 'react-router-ga'
 import registerServiceWorker from './registerServiceWorker'
+import Raven from 'raven-js'
+
+Raven.config(
+  'https://b9c8a706e38e4afb9d192c150edc5464@sentry.io/1263996'
+).install()
 
 render(
   <BrowserRouter>
